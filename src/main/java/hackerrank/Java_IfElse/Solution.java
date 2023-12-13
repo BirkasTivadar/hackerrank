0b20b2c3366f391712e1329b3e85720d6dc7ab72
BirkasTivadar <birkastivadar@gmail.com>
2023-12-13 19:57:05 +0100

    Java If-Else Kész

---
+++ src/main/java/hackerrank/Java_IfElse/Solution.java
@@ -0,0 +1,17 @@
+package hackerrank.Java_IfElse;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int myInt = scanner.nextInt();
+
+        if (myInt % 2 == 1 || (myInt >= 6 && myInt <= 20)) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+    }
+}
