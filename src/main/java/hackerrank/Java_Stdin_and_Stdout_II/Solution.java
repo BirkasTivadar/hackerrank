670c4ef59b4f90f720ed51225a60c29b2da9ee33
BirkasTivadar <birkastivadar@gmail.com>
2023-12-13 20:13:03 +0100

    Java Stdin and Stdout II Kész

---
+++ src/main/java/hackerrank/Java_Stdin_and_Stdout_II/Solution.java
@@ -0,0 +1,19 @@
+package hackerrank.Java_Stdin_and_Stdout_II;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int myInt = scanner.nextInt();
+        double myDouble = scanner.nextDouble();
+        scanner.nextLine();
+        String myString = scanner.nextLine();
+        scanner.close();
+
+        System.out.println("String: "+ myString);
+        System.out.println("Double: "+ myDouble);
+        System.out.println("Int: "+ myInt);
+    }
+}
