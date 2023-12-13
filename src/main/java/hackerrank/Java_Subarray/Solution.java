6d1c44da62a0448325a8bdc8a90efef09796959e
BirkasTivadar <birkastivadar@gmail.com>
2023-12-13 20:21:48 +0100

    for ciklus kiváltása IntStreammel

--- src/main/java/hackerrank/Java_Subarray/Solution.java
+++ src/main/java/hackerrank/Java_Subarray/Solution.java
@@ -2,18 +2,18 @@ package hackerrank.Java_Subarray;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int size = scanner.nextInt();
         int[] arrayOfNumbers = new int[size];
+        IntStream.range(0, size)
+                .forEach(i -> arrayOfNumbers[i] = scanner.nextInt());
 
-        for (int i = 0; i < size; i++) {
-            int number = scanner.nextInt();
-            arrayOfNumbers[i] = number;
-        }
         scanner.close();
 
         int counter = 0;
@@ -22,7 +22,6 @@ public class Solution {
             for (int j = i; j <= size; j++) {
                 int[] subArray = Arrays.copyOfRange(arrayOfNumbers, i, j);
                 if (Arrays.stream(subArray).sum() < 0) counter++;
-
             }
         }
         System.out.println(counter);
