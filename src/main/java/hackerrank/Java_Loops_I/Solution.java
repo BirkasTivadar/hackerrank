3a91fb0758c383c8d394abcabdae606c7555275d
BirkasTivadar <birkastivadar@gmail.com>
2023-12-14 21:39:22 +0100

    Java Loops I Kész

---
+++ src/main/java/hackerrank/Java_Loops_I/Solution.java
@@ -0,0 +1,17 @@
+package hackerrank.Java_Loops_I;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int myInt = scanner.nextInt();
+        scanner.close();
+
+        IntStream.range(1, 11)
+                .forEach(i -> System.out.println(String.format("%d x %d = %d", myInt, i, myInt * i)));
+    }
+}
+
